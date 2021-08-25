c8ceeb125b0a1cb599ffff5589b8c28e0b1b4d95
LiamTran1509 <trankhacquyet1996@gmail.com>
2021-08-25 19:47:32 +0700

    writing render function

--- main.js
+++ main.js
@@ -1,9 +1,26 @@
 const api = "https://corona.lmao.ninja/v2/countries?sort=country"
+const h1Element = document.querySelector('h1')
+console.log(h1Element)
 
-fetch(api)
-    .then((respone) => {
-        return respone.json()
-    })
-    .then((data) => {
-        console.log("🚀 ~ file: main.js ~ line 6 ~ .then ~ data", data)
-    })
+function start() {
+
+    // getCountry(data => {
+    //     console.log("🚀 ~ file: main.js ~ line 6 ~ start ~ data", data)
+    // })
+    getCountry(render)
+}
+
+function render(data) {
+    console.log(data[5])
+    const { cases, countryInfo, deaths, population, recovered, tests, todayCases, todayDeaths, todayRecovered } = data[5]
+}
+
+function getCountry(callback) {
+
+    fetch(api)
+        .then((respone) => {
+            return respone.json()
+        })
+        .then(callback)
+}
+start()
